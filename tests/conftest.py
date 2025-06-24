014d1fd47ee999f529ec7b51ba80643be752ccab
Kristián Kunc <kristian.kunc@gmail.com>
2025-06-24 20:14:07 +0000

    use db url even for tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,10 +6,10 @@ from tortoise.contrib.test import finalizer, initializer
 
 @pytest.fixture(scope="session", autouse=True)
 def initialize_tests(request):
-    db_url = os.environ.get("TEST_DATABASE_URL")
+    db_url = os.environ.get("DATABASE_URL")
     if not db_url:
-        raise ValueError("TEST_DATABASE_URL environment variable is not set or empty.")
-    os.environ["PGDATABASE"] = "devdb"  # TODO: grab this directly from the environment
+        raise ValueError("DATABASE_URL environment variable is not set or empty.")
+    os.environ["PGDATABASE"] = "testdb"  # TODO: grab this directly from the environment
 
     print(f"Using database URL: {db_url}")
 
