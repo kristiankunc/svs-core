7d37ebf5cfb2b3654c3b1a337889c833010098cf
Kristián Kunc <kristian.kunc@gmail.com>
2025-06-24 21:05:58 +0000

    take PGDATABASE from env directly

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,13 +6,14 @@ from tortoise.contrib.test import finalizer, initializer
 
 @pytest.fixture(scope="session", autouse=True)
 def initialize_tests(request):
-    db_url = os.environ.get("TEST_DATABASE_URL")
-    if not db_url:
+    test_db_url = os.environ.get("TEST_DATABASE_URL")
+    db_url = os.environ.get("DATABASE_URL")
+
+    if not db_url or not test_db_url:
         raise ValueError("TEST_DATABASE_URL environment variable is not set or empty.")
-    os.environ["PGDATABASE"] = "devdb"  # TODO: grab this directly from the environment
 
-    print(f"Using database URL: {db_url}")
+    os.environ["PGDATABASE"] = db_url.split("/")[-1]
 
-    initializer(["svs_core.db.models"], db_url=db_url, app_label="models")
+    initializer(["svs_core.db.models"], db_url=test_db_url, app_label="models")
 
     request.addfinalizer(finalizer)
