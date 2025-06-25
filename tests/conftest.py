51b72f3254854dfe841edb442e3d9fd184e5218d
Kristián Kunc <kristian.kunc@gmail.com>
2025-06-25 17:59:55 +0000

    add db fixture comments

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,12 +6,15 @@ from tortoise.contrib.test import finalizer, initializer
 
 @pytest.fixture(scope="session", autouse=True)
 def initialize_tests(request):
+    """Initialize the test database before running tests."""
     test_db_url = os.environ.get("TEST_DATABASE_URL")
     db_url = os.environ.get("DATABASE_URL")
 
     if not db_url or not test_db_url:
         raise ValueError("TEST_DATABASE_URL environment variable is not set or empty.")
 
+    # Sets the database which will be used for the connection as the testing database will be dropped
+    # and recreated through the test suite.
     os.environ["PGDATABASE"] = db_url.split("/")[-1]
 
     initializer(["svs_core.db.models"], db_url=test_db_url, app_label="models")
