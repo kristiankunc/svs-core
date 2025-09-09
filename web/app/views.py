f0bb1a351ffd60c795fb032fc1b65d218d14ffe6
Kristián Kunc <kristian.kunc@gmail.com>
2025-09-09 10:58:55 +0200

    add debug autologin

--- web/app/views.py
+++ web/app/views.py
@@ -3,9 +3,15 @@ from django.shortcuts import redirect, render
 
 from svs_core.docker.template import Template
 from svs_core.users.user import User
+from web import settings
 
 
 async def index(request):
+    if settings.DEBUG:
+        request.session["username"] = "testuser"
+        request.session["is_authenticated"] = True
+        request.session.set_expiry(3600)
+
     user = None
     if request.session.get("is_authenticated", False):
         user = await User.get_by_name(request.session["username"])
@@ -46,15 +52,23 @@ def logout(request):
 
 
 async def templates(request):
+    user = None
+    if request.session.get("is_authenticated", False):
+        user = await User.get_by_name(request.session["username"])
     templates = await Template.get_all()
-    return render(request, "templates.html", {"templates": templates})
+    return render(request, "templates.html", {"templates": templates, "user": user})
 
 
 async def template_detail(request, template_id):
     try:
+        user = None
+        if request.session.get("is_authenticated", False):
+            user = await User.get_by_name(request.session["username"])
         template = await Template.get_by_id(template_id)
         if not template:
             raise Http404("Template not found")
-        return render(request, "template_detail.html", {"template": template})
+        return render(
+            request, "template_detail.html", {"template": template, "user": user}
+        )
     except Exception as e:
         raise Http404(f"Template not found: {e}")
