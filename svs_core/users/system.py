dcfbd35355d829c7acc540910410b94bc82a1ec7
Kristián Kunc <kristian.kunc@gmail.com>
2025-09-17 15:31:42 +0200

    add system docs

--- svs_core/users/system.py
+++ svs_core/users/system.py
@@ -3,11 +3,14 @@ from svs_core.shared.shell import run_command
 
 class SystemUserManager:
     @staticmethod
-    def create_user(username: str, password: str) -> None:
+    def create_user(username: str, password: str, admin: bool = False) -> None:
         # Create user
         run_command(f"sudo useradd -m {username}", check=True)
         run_command(f"echo '{username}:{password}' | sudo chpasswd", check=True)
         run_command(f"sudo usermod -aG svs-users {username}", check=True)
+
+        if admin:
+            run_command(f"sudo usermod -aG svs-admins {username}", check=True)
 
     @staticmethod
     def delete_user(username: str) -> None:
