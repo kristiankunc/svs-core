24fd312e68ae6a6a738705f5c1c8e62ff8de8f60
Kristián Kunc <kristian.kunc@gmail.com>
2025-09-27 19:17:01 +0200

    System installation (#17)
    * add automatic testing releaser
    
    * modify testpypi repo settings
    
    * modify version setup
    
    * grab current ver via grep instead of py
    
    * use timestamps for pr builds
    
    * grant workflow comment perms
    
    * test pypi version bumping
    
    * properly update test pypi comment
    
    * run test release only after passing unit tests
    
    * update test pypi install url
    
    * include svs_core in build
    
    * run package release only after tests
    
    * add setup script
    
    * include git sha in pypi release msg
    
    * properly create temp db url
    
    * use base exceptions for setup script
    
    * run all commands in shell mode
    
    * add datetime to pypi build
    
    * update shell unit tests
    
    * add setup user to admins group
    
    * fix commit name
    
    * grab current user properly

--- svs_core/__main__.py
+++ svs_core/__main__.py
@@ -5,20 +5,27 @@ import os
 
 import typer
 
+from svs_core.shared.logger import get_logger
+
 if not os.getenv("DATABASE_URL"):
     from dotenv import load_dotenv
 
     load_dotenv()
 
-# from svs_core.cli.service import app as service_app
-# from svs_core.cli.template import app as template_app
+    # TODO: Figure out a better way to handle initial configurations
+    if not os.getenv("DATABASE_URL"):
+        os.environ["DATABASE_URL"] = "postgres://a:a@localhost:1234/testdb"
+        get_logger(__name__).warning(
+            "DATABASE_URL environment variable not set. Running detached from database."
+        )
+
+from svs_core.cli.setup import app as setup_app
 from svs_core.cli.user import app as user_app
 
 app = typer.Typer(help="SVS CLI")
 
 app.add_typer(user_app, name="user")
-# app.add_typer(template_app, name="template")
-# app.add_typer(service_app, name="service")
+app.add_typer(setup_app, name="setup")
 
 
 def main() -> None:
