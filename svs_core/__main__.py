3f87ce5391835b3e40fac2d8e95255033ee05a6c
Kristián Kunc <kristian.kunc@gmail.com>
2025-10-22 09:40:24 +0000

    add env manager

--- svs_core/__main__.py
+++ svs_core/__main__.py
@@ -23,10 +23,10 @@ if not os.getenv("DATABASE_URL"):
             "DATABASE_URL environment variable not set. Running detached from database."
         )
 
-from svs_core.cli.service import app as service_app
-from svs_core.cli.setup import app as setup_app
-from svs_core.cli.template import app as template_app
-from svs_core.cli.user import app as user_app
+from svs_core.cli.service import app as service_app  # noqa: E402
+from svs_core.cli.setup import app as setup_app  # noqa: E402
+from svs_core.cli.template import app as template_app  # noqa: E402
+from svs_core.cli.user import app as user_app  # noqa: E402
 
 app = typer.Typer(help="SVS CLI", pretty_exceptions_enable=False)
 
@@ -35,9 +35,38 @@ app.add_typer(setup_app, name="setup")
 app.add_typer(template_app, name="template")
 app.add_typer(service_app, name="service")
 
+IS_ADMIN = False
+
 
 def main() -> None:  # noqa: D103
-    get_logger(__name__).debug(f"{getpass.getuser()} ran: {' '.join(sys.argv)}")
+    from svs_core.users.system import SystemUserManager  # noqa: E402
+    from svs_core.users.user import User  # noqa: E402
+
+    username = getpass.getuser()
+    user = User.objects.filter(name=username).first()
+
+    if not user and sys.argv[1] != "setup":
+        print(
+            f"You are running as system user '{username}', but no matching SVS user was found."
+        )
+        get_logger(__name__).warning(
+            f"User '{username}' tried to run CLI but was not found."
+        )
+
+        if SystemUserManager.is_user_in_group(username, "svs-admins"):
+            print(
+                "You appear to be an admin user without an SVS user account. Please create your SVS user via: svs user create"
+            )
+        else:
+            sys.exit(1)
+
+    if user and user.is_admin():
+        global IS_ADMIN
+        IS_ADMIN = True
+
+    get_logger(__name__).debug(
+        f"{user if user is not None else username} ({('admin' if IS_ADMIN else 'standard user')}) ran: {' '.join(sys.argv)}"
+    )
     app()
 
 
