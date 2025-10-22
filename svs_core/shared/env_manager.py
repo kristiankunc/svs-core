03bbb9d2e5bae8839604fd27f613cc294fa4f4db
Kristián Kunc <kristian.kunc@gmail.com>
2025-10-22 09:49:24 +0000

    allow loading of additional env vars

--- svs_core/shared/env_manager.py
+++ svs_core/shared/env_manager.py
@@ -46,6 +46,35 @@ class EnvManager:
             return cls.RuntimeEnvironment.DEVELOPMENT
 
     @classmethod
+    def _open_env_file(cls, path: Path) -> dict[str, str]:
+        """Opens and reads the .env file at the specified path.
+
+        Args:
+            path (Path): The path to the .env file.
+
+        Returns:
+            dict[str, str]: A dictionary of environment variables.
+
+        Raises:
+            FileNotFoundError: If the .env file does not exist.
+        """
+
+        env_vars = {}
+
+        try:
+            with open(path, "r") as env_file:
+                for line in env_file:
+                    if line.strip() and not line.startswith("#"):
+                        key, value = line.strip().split("=", 1)
+                        env_vars[key] = value
+
+        except FileNotFoundError as e:
+            get_logger(__name__).error(f"{path} not found.")
+            raise e
+
+        return env_vars
+
+    @classmethod
     def _read_env(cls) -> MappingProxyType[str, str]:
         """Reads environment variables from the .env file and caches them.
 
@@ -60,18 +89,29 @@ class EnvManager:
             env_vars = {}
 
             try:
-                with open(cls.ENV_FILE_PATH, "r") as env_file:
-                    for line in env_file:
-                        if line.strip() and not line.startswith("#"):
-                            key, value = line.strip().split("=", 1)
-                            env_vars[key] = value
-
+                env_vars = cls._open_env_file(cls.ENV_FILE_PATH)
             except FileNotFoundError as e:
                 get_logger(__name__).error(f"{cls.ENV_FILE_PATH} not found.")
                 raise e
 
             cls._env_cache_internal = env_vars
             cls._env_cache = MappingProxyType(env_vars)
+
+            if cls.get_runtime_environment() == cls.RuntimeEnvironment.DEVELOPMENT:
+                get_logger(__name__).info(
+                    "Running in DEVELOPMENT environment as per .env configuration, loading local .env"
+                )
+
+                local_env_path = Path(".env")
+
+                try:
+                    local_env_vars = cls._open_env_file(local_env_path)
+                    cls._env_cache_internal.update(local_env_vars)
+                    cls._env_cache = MappingProxyType(cls._env_cache_internal)
+                except FileNotFoundError:
+                    get_logger(__name__).warning(
+                        f"Local .env file {local_env_path} not found."
+                    )
 
         cls._env_loaded = True
         return cls._env_cache
